e7b3635d3ad8c67a1bf32a0accd4991eba29fadb
damaschele <damaschele@gmail.com>
2023-05-27 14:38:44 +0200

    Web-Django

---
+++ venv/authentication/mainapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('dashboard/',views.dashboard, name = 'dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LoginView.as_view(), name ='logout'),
+    
+    
+    #password change
+    path('password_change', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
